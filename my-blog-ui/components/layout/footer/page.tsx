f9f8e3635c0cdc9d22e7cfb39c0695c0738b5f3c
Ayumu3746221 <a.kkt0307@gmail.com>
2024-11-08 18:13:10 +0900

    Create Footer

---
+++ my-blog-ui/components/layout/footer/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-[#0E397B] py-4 text-center">
+      <p className="text-[#D7BC61]">ayumu log - Tech Blog for ayumu</p>
+    </footer>
+  );
+};
+
+export default Footer;
