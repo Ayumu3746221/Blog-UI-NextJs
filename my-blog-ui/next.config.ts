0bacf93564e911153403318febcc1b24f0c13c5b
Ayumu3746221 <a.kkt0307@gmail.com>
2024-12-07 14:15:58 +0900

    fetch: add blog data fetching from backend

--- my-blog-ui/next.config.ts
+++ my-blog-ui/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/code_infinite_datas/**",
+      },
+    ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
